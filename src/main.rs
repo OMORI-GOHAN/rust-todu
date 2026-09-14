b2a440b13ca5a16e7693427bc0b2b53b64733c39
Gohan_taberu <Gohan_taberu@proton.me>
2026-09-14 16:55:13 +0800

    feat: implement task CRUD

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use std::io;
 use chrono::Local;
  
 
-#[derive(Clone)]
 struct Task {   
     id: u64,
     description: String,
@@ -27,7 +26,7 @@ impl Task {
     }
 }
 
-fn add(tasks:&mut Vec<Task>) {
+fn add(tasks: &mut Vec<Task>) {
     // 添加description
     println!("输入添加的任务");
     let mut description = String::new();
@@ -50,16 +49,24 @@ fn add(tasks:&mut Vec<Task>) {
 }
 
 // 查看任务
-fn show_tasks(tasks:&[Task]) {
+fn show_tasks(tasks: &[Task]) {
     for item in tasks {
         item.show();
     }
 }
 
 // 删除任务
-fn delete(tasks:&mut Vec<Task>) {
+fn delete(tasks: &mut Vec<Task>) {
     show_tasks(tasks);
     println!("输入你想要删除的任务的描述/ID/创建时间");
+    let local = find_task(tasks).expect("找不到该任务");
+
+
+    tasks.remove(local);
+    println!("已删除!");
+}
+
+fn find_task(tasks: &[Task]) -> Option<usize> {
     let mut target = String::new();
     io::stdin()
         .read_line(&mut target)
@@ -81,16 +88,53 @@ fn delete(tasks:&mut Vec<Task>) {
         }
         _ => {
             println!("无效的查找类型");
-            return
+            return None
         }
     };
+    Some(local)
+}
 
-    tasks.remove(local);
-    println!("已删除!");
+fn change_task(tasks: &mut Vec<Task>) {
+    show_tasks(tasks);
+    println!("输入你想要修改的任务的描述/ID/创建时间");
+    let local = find_task(tasks).expect("找不到该任务");
+
+    tasks[local].show();
+    println!("
+    选择您要执行的操作：
+    1.修改任务描述
+    2.修改任务完成与否
+    ");
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("请输入合法内容");
+    let input = input.trim();
+    match input {
+        "1" => {
+            println!("请输入新的任务描述");
+            let mut new_description = String::new();
+            io::stdin()
+                .read_line(&mut new_description)
+                .expect("请输入合法内容");
+            let new_description = new_description.trim();
+            tasks[local].description = new_description.to_string();
+
+        },
+        "2" => {
+            tasks[local].completed = !tasks[local].completed;
+        },
+        _ => {
+            println!("请输入合法内容！");
+        }
+    }
+
+    println!("已修改!");
+
 }
 
 // 检查输入的是id/描述/时间
-fn check_type(target:&str) -> &str {
+fn check_type(target: &str) -> &str {
     let chars: Vec<char> = target.chars().collect();
     let string_type:&str;
     
@@ -123,7 +167,7 @@ fn check_type(target:&str) -> &str {
 
 
 // 查找方法(id) by 二分查找
-fn search_by_id(tasks:&[Task], id:u64) -> Option<usize> {
+fn search_by_id(tasks: &[Task], id: u64) -> Option<usize> {
     let mut left = 0;
     let mut right   = tasks.len();
     loop {
@@ -141,7 +185,7 @@ fn search_by_id(tasks:&[Task], id:u64) -> Option<usize> {
     Some((left + right) / 2)
 }
 // 查找方法(date)
-fn search_by_date(tasks:&[Task], date:&str) -> Option<usize> {
+fn search_by_date(tasks: &[Task], date: &str) -> Option<usize> {
 
     for (index, task) in tasks.iter().enumerate(){
         if task.date == date {
@@ -152,7 +196,7 @@ fn search_by_date(tasks:&[Task], date:&str) -> Option<usize> {
 }
 
 // 查找方法(description)
-fn search_by_description(tasks:&[Task], description:&str) -> Option<usize> {
+fn search_by_description(tasks: &[Task], description: &str) -> Option<usize> {
 
     for (index, task) in tasks.iter().enumerate() {
         if task.description == description {
@@ -162,13 +206,14 @@ fn search_by_description(tasks:&[Task], description:&str) -> Option<usize> {
     None
 }
 
-fn control(tasks:&mut Vec<Task>) -> bool {
+fn control(tasks: &mut Vec<Task>) -> bool {
     print!("
     欢迎使用rust-task!
-    请输入对应数组以选择模式
+    请输入对应数字以选择模式
     1.添加任务
     2.删除任务
     3.查看任务
+    4.修改任务
     
     Q.退出
     ");
@@ -192,7 +237,10 @@ fn control(tasks:&mut Vec<Task>) -> bool {
             show_tasks(tasks);
             true
         },
-
+        "4" => {
+            change_task(tasks);
+            true
+        }
         "Q" | "q" => {
             println!("退出任务");
             false
@@ -208,7 +256,7 @@ fn control(tasks:&mut Vec<Task>) -> bool {
 
 
 fn main() {
-    let mut tasks:Vec<Task> = Vec::new();
+    let mut tasks: Vec<Task> = Vec::new();
 
     loop {
         if !control(&mut tasks) {
